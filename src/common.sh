e46e0d517ff9cd2e4a9f3e28cbdeb6a3ad2806de
Bryan Weaver <bryanweaver@outlook.com>
2017-03-31 01:21:01 -0500

    removing internal TFS repo functionality.  This is already handled by TFS

--- src/common.sh
+++ src/common.sh
@@ -19,33 +19,3 @@ function updateWitHistoryWithExternalGit() {
 
     curl -u ${vstsUser}:${vstsPat} --request PATCH --header "$header" --data "$payload" ${url}
 }
-
-function updateWitHistoryWithTfsGit() {
-    local account="${1}"
-    local collection="${2}"
-    local id="${3}"
-    local apiVersion="${4}"
-    local commit="${5}"
-    local cloneUrl="${6}"
-    local vstsUser="${7:-$VSTS_USER}"
-    local vstsPat="${8:-$VSTS_PAT}"
-
-    local repoInfo=$(getVstsGitRepoInfo "${cloneUrl}" "${vstsUser}" "${vstsPat}")
-    local projectId=${repoInfo} | jq .repository.project.id
-    local repoId=${repoInfo} | jq .repository.id
-
-    # Update Work Item API Documentation: https://www.visualstudio.com/en-us/docs/integrate/api/wit/work-items#update-work-items
-    local header="Content-Type: application/json-patch+json"
-    local payload='[{"op": "add","path": "/relations/-","value": { "rel": "ArtifactLink", "url": "vstfs://Git/Commit/'${projectId}'%2F'${repoId}'%2F'${commit}'" }}]'
-    local url="https://${account}.VisualStudio.com/${collection}/_apis/wit/workitems/${id}?api-version=${apiVersion}"
-
-    curl -u ${vstsUser}:${vstsPat} --request PATCH --header \"${header}\" --data '${payload}' ${url}
-}
-
-function getVstsGitRepoInfo() {
-    local cloneUrl="${1}"
-    local vstsUser="${2:-$VSTS_USER}"
-    local vstsPat="${3:-$VSTS_PAT}"
-
-    curl -u ${vstsUser}:${vstsPat} "${cloneUrl}/vsts/info"
-}
--- test/runtests.sh
+++ test/runtests.sh
@@ -4,7 +4,5 @@ source helper.sh
 source common.test
 
 echo $(updateWitHistoryWithExternalGit_Should_Return_Correct_JSON)
-echo $(getVstsGitRepoInfo_Should_Return_Correct_JSON)
-echo $(updateWitHistoryWithTfsGit_Should_Return_Correct_Result)
 
 echo -e "Tests complete"
