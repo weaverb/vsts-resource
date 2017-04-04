e662500698c438bce11da7605ab9f6bc3942bda1
Bryan Weaver <bryanweaver@outlook.com>
2017-04-03 22:28:58 -0500

    updating new tests for curl options

--- test/runtests.sh
+++ test/runtests.sh
@@ -4,5 +4,8 @@ source helper.sh
 source common.test
 
 echo $(updateWitHistoryWithExternalGit_Should_Return_Correct_JSON)
+echo $(updateWitHistoryWithExternalGit_Should_Return_Correct_JSON_Curl_Insecure)
+echo $(updateWitHistoryWithExternalGit_Should_Return_Correct_JSON_Curl_Ntlm)
+echo $(updateWitHistoryWithExternalGit_Should_Return_Correct_JSON_Curl_Ntlm_Insecure)
 
 echo -e "Tests complete"
